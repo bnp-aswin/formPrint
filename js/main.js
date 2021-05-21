42b107d818492446cfd996ff2965e556d83275ba
aswinblix <aswin.blix@bitsnpixs.com>
2021-05-21 11:24:36 +0530

    Whole file modified

--- js/main.js
+++ js/main.js
@@ -1,49 +1,31 @@
+function formPrint() {
 
-    var fname= [];
-        var lname = [];
-        var code = [];
-        var dob = [];
-         var desig = [];
+    var myForm = document.getElementById('employeeDetails');
+    var firstName = document.getElementById('fname').value;
+    var lastName = document.getElementById('lname').value;
+    var employeeCode = document.getElementById('code').value;
+    var dateofBirth = document.getElementById('dob').value;
+    var designation = document.getElementById('designation').value;
+    if (firstName == "" && lastName == "" && employeeCode == "" && dateofBirth == "" && designation == "0") {
+        alert('Please fill all fields');
+    } else {
+        var printTable = document.getElementById('prinTable');
+        var inRow = printTable.insertRow(1);
+        var fname = inRow.insertCell(0);
+        var lname = inRow.insertCell(1);
+        var code = inRow.insertCell(2);
+        var dob = inRow.insertCell(3);
+        var desig = inRow.insertCell(4);
 
-        var a = 1;
-        var b = 0;
-
-        function AddRow() {
-
-            var container = document.getElementById('formDetails');
-            var addRow = container.insertRow(a);
-            var designation=document.employee.designation.value;
-
-if(designation=="select"){
-    alert("Plz select the designation")
-    return false;
+        fname.innerHTML = firstName;
+        lname.innerHTML = lastName;
+        code.innerHTML = employeeCode;
+        dob.innerHTML = dateofBirth;
+        desig.innerHTML = designation;
+        myForm.reset();
+    }
 }
-else if(designation=="Software Engineer"){
-    designation="Software Engineer"
-
-}
-else if(designation=="Web Developer"){
-    designation="Web Developer"
-
-}
-            fname[b] = document.getElementById("fname").value;
-            lname[b] = document.getElementById("lname").value;
-            code[b] = document.getElementById("code").value;
-            dob[b] = document.getElementById("dob").value;
-            desig[b] = designation;
-
-            var cell1 = addRow.insertCell(0);
-            var cell2 = addRow.insertCell(1);
-            var cell3 = addRow.insertCell(2);
-            var cell4 = addRow.insertCell(3);
-            var cell5 = addRow.insertCell(4);
-
-            cell1.innerHTML = fname[b];
-            cell2.innerHTML = lname[b];
-            cell3.innerHTML = code[b];
-            cell4.innerHTML = dob[b];
-            cell5.innerHTML = desig[b];
-            a++;
-            b++;
-        }
-
+document.getElementById('btn-save').addEventListener('click', function (event) {
+    event.preventDefault();
+});
+document.getElementById('btn-save').addEventListener('click', formPrint);
