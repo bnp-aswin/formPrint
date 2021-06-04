bbe2c083d3bbe09eab6930938e9fc6ab4bc757fd
aswinblix <aswin.blix@bitsnpixs.com>
2021-06-04 11:21:44 +0530

    Whole file modified with read and write form data

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-function formPrint() {
 
-    var myForm = document.getElementById('employeeDetails');
-    var firstName = document.getElementById('fname').value;
-    var lastName = document.getElementById('lname').value;
-    var employeeCode = document.getElementById('code').value;
-    var dateofBirth = document.getElementById('dob').value;
-    var designation = document.getElementById('designation').value;
-    if (firstName == "" && lastName == "" && employeeCode == "" && dateofBirth == "" && designation == "0") {
-        alert('Please fill all fields');
-    } else {
-        var printTable = document.getElementById('prinTable');
-        var inRow = printTable.insertRow(1);
-        var fname = inRow.insertCell(0);
-        var lname = inRow.insertCell(1);
-        var code = inRow.insertCell(2);
-        var dob = inRow.insertCell(3);
-        var desig = inRow.insertCell(4);
+function readData() {
+    var employeeData = {};
+    employeeData['firstName'] = document.getElementById('first-name').value;
+    employeeData['lastName'] = document.getElementById('last-name').value;
+    employeeData['employeeCode'] = document.getElementById('employee-code').value;
+    employeeData['dateOfBirth'] = document.getElementById('dob').value;
+    employeeData['designation'] = document.getElementById('designation').value;
+    return employeeData;
 
-        fname.innerHTML = firstName;
-        lname.innerHTML = lastName;
-        code.innerHTML = employeeCode;
-        dob.innerHTML = dateofBirth;
-        desig.innerHTML = designation;
-        myForm.reset();
-    }
+}
+
+function writeData(record) {
+    var tableBody = document.getElementsByTagName('tbody')[0];
+    var newRow = tableBody.insertRow(tableBody.length);
+    newRow.insertCell(0).innerHTML = record.firstName;
+    newRow.insertCell(1).innerHTML = record.lastName;
+    newRow.insertCell(2).innerHTML = record.employeeCode;
+    newRow.insertCell(3).innerHTML = record.dateOfBirth;
+    newRow.insertCell(4).innerHTML = record.designation;
+    newRow.insertCell(5).innerHTML = '<a onClick="">Edit</a>'
+    newRow.insertCell(6).innerHTML = '<a onClick="">Delete</a>'
+
 }
 document.getElementById('btn-save').addEventListener('click', function (event) {
     event.preventDefault();
+    var formDetails = readData();
+    writeData(formDetails);
 });
-document.getElementById('btn-save').addEventListener('click', formPrint);
